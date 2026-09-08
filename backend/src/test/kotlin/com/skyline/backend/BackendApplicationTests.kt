ee078a2679cb972c82d99e2741e76b061212f733
Polina Brazhnyk <polinabrazhnyk@gmail.com>
2026-09-08 12:42:06 +0300

    Initial project setup

---
+++ backend/src/test/kotlin/com/skyline/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skyline.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
